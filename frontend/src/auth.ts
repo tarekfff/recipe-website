54ea2008228a3aa9fc8200a9b714fcca9025b638
tarekfff <tarekchadi66@gmail.com>
2026-03-30 11:14:40 -0700

    enable trustHost in NextAuth configuration

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -10,6 +10,7 @@ const credentialsSchema = z.object({
 })
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    trustHost: true,
     providers: [
         Credentials({
             credentials: {
